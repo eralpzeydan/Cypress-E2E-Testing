5a00a0ed6a855e0183e9d0f00c6f844d093259dd
eralpzeydan <eralpzeydan@gmail.com>
2023-11-12 16:36:13 +0100

    sharing location stubs,fakecalls, timeout

--- 05 stubs-starting-project/cypress/e2e/location.cy.js
+++ 05 stubs-starting-project/cypress/e2e/location.cy.js
@@ -2,7 +2,24 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then( (win) => {
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as(
+        'getUserPosition'
+        ).callsFake( (cb) => {
+          setTimeout( () => {
+            cb({
+              coords: {
+                latidute: 37.5,
+                longitude: 48.01,
+              }
+            });
+          }, 100)
+        });
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called');
+    //button is disabled
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain','Location fetched!')
   });
 });
